0aa06b91e6633ede0a5be659765567fcaa48d04a
leiyuancheng <152260388+leiyuancheng@users.noreply.github.com>
2024-05-28 17:45:31 +0800

    Update BanditBasedLearning.m

--- BanditBasedLearning.m
+++ BanditBasedLearning.m
@@ -1,14 +1,9 @@
 function [q,k]=BanditBasedLearning(r,r_average,bestIndex,k,LLH)
     
-    
     r_average(bestIndex)=(k(bestIndex)*r_average(bestIndex)+r(bestIndex))/(k(bestIndex)+1);
     k(bestIndex)=k(bestIndex)+1;
 
-    %对每个LLH都更新q
     for i=1:LLH
-        
         q(i)=cUCB(r_average(i),k,i);
-
     end
-    
 end
